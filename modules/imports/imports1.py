eb82637137ead9c9e74fff880f6ce7d0882e1701
Suraj N <surajn@mactores.com>
2022-03-07 18:20:20 +0530

    Updated Steps and added imports section

---
+++ modules/imports/imports1.py
@@ -0,0 +1,18 @@
+import package1
+package1.module1.Class1
+package1.module1.func1
+
+
+import package1.module1
+module1.Class1
+module1.func1
+
+
+from package1 import mmodule1
+module1.Class1
+module1.func1
+
+
+from package1.module1 import Class1,func1
+Class1
+func1
