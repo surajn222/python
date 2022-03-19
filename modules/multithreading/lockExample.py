f92531947e45902b5e4ba990a52030db495c03bb
Suraj N <surajn@mactores.com>
2022-03-19 17:21:20 +0530

    Updated Steps and added imports section

---
+++ modules/multithreading/lockExample.py
@@ -0,0 +1,34 @@
+from threading import Thread
+from time import sleep
+
+
+counter = 0
+
+
+def increase(by):
+    global counter
+
+    local_counter = counter
+    local_counter += by
+
+    sleep(0.1)
+
+    counter = local_counter
+    print(f'counter={counter}')
+
+
+# create threads
+t1 = Thread(target=increase, args=(10,))
+t2 = Thread(target=increase, args=(20,))
+
+# start the threads
+t1.start()
+t2.start()
+
+
+# wait for the threads to complete
+t1.join()
+t2.join()
+
+
+print(f'The final counter is {counter}')
---
+++ modules/multithreading/lockExample2.py
@@ -0,0 +1,40 @@
+from threading import Thread, Lock
+from time import sleep
+
+
+counter = 0
+
+
+def increase(by, lock):
+    global counter
+
+    lock.acquire()
+
+    local_counter = counter
+    local_counter += by
+
+    sleep(0.1)
+
+    counter = local_counter
+    print(f'counter={counter}')
+
+    lock.release()
+
+
+lock = Lock()
+
+# create threads
+t1 = Thread(target=increase, args=(10, lock))
+t2 = Thread(target=increase, args=(20, lock))
+
+# start the threads
+t1.start()
+t2.start()
+
+
+# wait for the threads to complete
+t1.join()
+t2.join()
+
+
+print(f'The final counter is {counter}')
